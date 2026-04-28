af29f78a17a3c1c5baf0649dc0ac73795c89a8f4
FT1E <ft1hcfu@gmail.com>
2026-04-28 14:46:32 +0200

    Added heap for making huffman tree and huffman tree made using the heap

---
+++ heap.py
@@ -0,0 +1,60 @@
+# heap with custom elements for making the huffman tree
+
+
+class Heap:
+    def __init__(self):
+        self.heap = []
+
+    def insert(self, node):
+        self.heap.append(node)
+        current = len(self.heap) - 1
+
+        while current >= 0:
+            parent = self.get_parent_index(current)
+
+            # if current node has probability smaller than parent
+            if self.heap[current].probability < self.heap[parent].probability:
+                self.heap[current], self.heap[parent] = self.heap[parent], self.heap[current]
+                current = parent
+            else:
+                break
+
+    def get_min(self):
+        return self.heap[0]
+    
+    def remove_min(self):
+        current = 0
+        while True:
+            left = self.get_left_child_index(current)
+            right = self.get_right_child_index(current)
+
+            if left >= len(self.heap):
+                self.heap[current], self.heap[-1] = self.heap[-1], self.heap[current]
+                self.heap.pop()
+                break
+            elif right >= len(self.heap):
+                self.heap[current], self.heap[left] = self.heap[left], self.heap[current]
+                self.heap.pop()
+                break
+            else:
+                if self.heap[left].probability <= self.heap[right].probability:
+                    self.heap[current], self.heap[left] = self.heap[left], self.heap[current]
+                    current = left
+                else:
+                    self.heap[current], self.heap[right] = self.heap[right], self.heap[current]
+                    current = right
+            
+
+    def extract_min(self):
+        min = self.get_min()
+        self.remove_min()
+        return min
+    
+    def get_parent_index(self, index):
+        return (index - 1) // 2
+    
+    def get_left_child_index(self, index):
+        return 2 * index + 1
+    
+    def get_right_child_index(self, index):
+        return 2 * index + 2
---
+++ tree.py
@@ -0,0 +1,64 @@
+
+# todo - making the huffman tree (using heap - bcs it's easier)
+# todo - encoding function - dictionary with direct access to leaves
+# todo - decoding function - traversing from the root of the tree to a leaf and then repeat until all symbols are consumed
+
+from heap import Heap
+
+class Node:
+    def __init__(self, symbol=None, probability=None, left=None, right=None):
+        self.symbol = symbol
+        self.probability = probability
+        self.left = left
+        self.right = right
+        self.encoding = None
+
+    
+
+class Tree:
+    def __init__(self, root=None):
+        self.root = root
+
+    # traverse the tree and assign encodings to the leaves
+    def assign_encodings(self, node=None, encoding=""):
+        if node is None:
+            node = self.root
+
+        if node.symbol is not None:
+            node.encoding = encoding
+            return
+        
+        self.assign_encodings(node.left, encoding + "0")
+        self.assign_encodings(node.right, encoding + "1")
+
+
+
+def make_tree(nodes):
+    heap = Heap()
+    for node in nodes:
+        heap.insert(node)
+
+    while len(heap.heap) > 1:
+        right = heap.extract_min()
+        left = heap.extract_min()
+
+        new_node = Node(symbol=None, probability=left.probability + right.probability, left=left, right=right)
+        heap.insert(new_node)
+    
+    tree = Tree(root=heap.extract_min())
+    tree.assign_encodings()
+    return tree
+
+
+SYMBOLS = ['a', 'b', 'c', 'd', 'e', 'f']
+PROBABILITIES = [0.05, 0.1, 0.15, 0.18, 0.22, 0.3]
+
+alphabet = []
+for i in range(6):
+    for j in range(6):
+        alphabet.append(Node(symbol=SYMBOLS[i] + SYMBOLS[j], probability=PROBABILITIES[i] * PROBABILITIES[j]))
+
+
+HuffmanTree = make_tree(alphabet)
+HuffmanTree.assign_encodings()
+Leaves = alphabet
